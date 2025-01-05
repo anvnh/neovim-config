1d3a881728a79cd16e8cc37b2952c3116a6e5014
anvnh <anvo20052@gmail.com>
2025-01-05 15:25:35 +0700

    Adding snippets for competitive programming using LuaSnip

---
+++ lua/custom/plugins/luasnip.lua
@@ -0,0 +1,14 @@
+return {
+  'L3MON4D3/LuaSnip',
+  dependencies = {
+    'saadparwaiz1/cmp_luasnip',
+    'rafamadriz/friendly-snippets',
+  },
+
+  -- Snippet location
+  config = function()
+    require('luasnip.loaders.from_lua').load()
+    require('luasnip.loaders.from_lua').lazy_load { paths = vim.g.lua_snippets_path or '~/.config/nvim/lua/custom/snippets/' }
+    require('luasnip.loaders.from_lua').lazy_load { paths = vim.g.lua_snippets_path or '~/.config/nvim/lua/custom/snippets/cp/' }
+  end,
+}
