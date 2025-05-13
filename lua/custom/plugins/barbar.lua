3b9767d9bed225451c78a9797e0a4877211eecbb
anvnh <anvo20052@gmail.com>
2025-05-13 22:42:47 +0700

    Adding offset of barbar to nvimtree and undotree

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -5,4 +5,19 @@ return {
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
   version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  config = function()
+    require('barbar').setup {
+      sidebar_filetypes = {
+        NvimTree = {
+          is_sidebar = true,
+          text = 'File Explorer',
+          align = 'center',
+        },
+        undotree = {
+          text = 'Undotree',
+          align = 'center',
+        },
+      },
+    }
+  end,
 }
