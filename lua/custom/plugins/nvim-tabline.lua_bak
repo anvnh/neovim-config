f8d8b00a9a4153eca9a06934b9cb4246f69af359
anvnh <anvo20052@gmail.com>
2025-07-29 23:11:32 +0700

    Commit

--- lua/custom/plugins/nvim-tabline.lua_bak
+++
@@ -1,5 +0,0 @@
-return {
-  'crispgm/nvim-tabline',
-  dependencies = { 'nvim-tree/nvim-web-devicons' }, -- optional
-  config = true,
-}
--- lua/custom/plugins/themes.lua
+++ lua/custom/plugins/themes.lua
@@ -1,26 +1,24 @@
 return {
-  {
-    'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'tokyonight'
-    end,
-  },
-  {
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    config = function()
-      vim.cmd.colorscheme 'catppuccin'
-    end,
-  },
+  -- {
+  --   'folke/tokyonight.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'tokyonight'
+  --   end,
+  -- },
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   config = function()
+  --     vim.cmd.colorscheme 'catppuccin'
+  --   end,
+  -- },
   {
     'ellisonleao/gruvbox.nvim',
     lazy = true,
     config = function()
       vim.cmd.colorscheme 'gruvbox'
-      vim.cmd [[
-            ]]
     end,
   },
 }
